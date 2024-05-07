c258ab7e22352a6462a69d5bce8fc25c31b04345
Jacky Z <yokiming1994@gmail.com>
2024-05-07 21:33:23 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    await invoke("my_custom_command")
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <h1>Tauri-template-JackyZ</h1>
+
+      <div className="row">
+        <p>11</p>
+        <p>22</p>
+        <p>33</p>
+      </div>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
